7250ef8c7b10dc9baf0b6d8b54514246f323ca2b
ezevallos <ezevallos@users.noreply.github.com>
2019-04-25 16:27:34 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -2,6 +2,21 @@
 """Proyecto CC471 - Analisis Bioinformatico de Especies
 """
 
+import os
+import sys
 import Bio
 from PyQt4 import QtGui
 from Bio.Seq import Seq
+
+def window():
+ app = QtGui.QApplication(sys.argv)
+ w = QtGui.QWidget()
+ b= QtGui.QLabel(w)
+ b.setText("Aquí construimos nuestra app!")
+ w.setGeometry(100,100,200,50)
+ b.move(50,20)
+ w.setWindowTitle(“Proyecto CC471 - Análisis Bioinformático de Especies”)
+ w.show()
+ sys.exit(app.exec_())
+if __name__ == '__main__':
+ window()
