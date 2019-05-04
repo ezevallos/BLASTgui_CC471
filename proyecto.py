f0cf737033dd9411f43bf12dee75ff87322eeac8
ezevallos <ezevalloslabarthe@gmail.com>
2019-05-03 19:24:14 -0500

    Cambios a la gui

--- proyecto.py
+++ proyecto.py
@@ -4,21 +4,13 @@
 
 import os
 import sys
-import plotly.plotly as py
-import plotly.figure_factory as ff
-import Bio
-from PyQt4 import QtGui
-from Bio.Seq import Seq
 
-def window():
- app = QtGui.QApplication(sys.argv)
- w = QtGui.QWidget()
- b= QtGui.QLabel(w)
- b.setText("Aquí construimos nuestra app!")
- w.setGeometry(100,100,200,50)
- b.move(50,20)
- w.setWindowTitle(“Proyecto CC471 - Análisis Bioinformático de Especies”)
- w.show()
- sys.exit(app.exec_())
-if __name__ == '__main__':
- window()
+# import Bio
+import tkinter as tk
+# from Bio.Seq import Seq
+
+win = tk.Tk()
+
+win.title("Python GUI")
+
+win.mainloop()
